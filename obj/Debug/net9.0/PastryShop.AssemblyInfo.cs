d31d24591aa08b25a9bbe0e2faa6fa599053741f
Mehdi Heidari <hymehdi84@gmail.com>
2025-02-05 00:11:39 +0300

    View imports created

--- obj/Debug/net9.0/PastryShop.AssemblyInfo.cs
+++ obj/Debug/net9.0/PastryShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PastryShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3ddaa4b8455f2bffefc76edcccea98d67064768")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2a0a2c3f9de7950b4de932e2a8d23c09e7fbcae")]
 [assembly: System.Reflection.AssemblyProductAttribute("PastryShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PastryShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
