a73b5c316ae9e14e0fa7f14207d56a6904dd51f9
Mehdi Heidari <hymehdi84@gmail.com>
2025-02-04 19:47:20 +0300

    first configuration is done

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
+builder.Services.AddControllersWithViews();
 app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
 
+app.MapDefaultControllerRoute();
 app.Run();
