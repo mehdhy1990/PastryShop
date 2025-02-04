b5ec855e53c4a9ddd1c4f63fbe13594c553969dc
Mehdi Heidari <hymehdi84@gmail.com>
2025-02-04 21:08:29 +0300

    mock category and pie repository are injected

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+using PastryShop.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
 app.MapGet("/", () => "Hello World!");
 app.UseStaticFiles();
 if (app.Environment.IsDevelopment())
