747d761e478b989c31b894b268c98ce113645b5b
Mehdi Heidari <hymehdi84@gmail.com>
2025-02-05 03:28:06 +0300

    all configuration for entity framework is done

---
+++ Models/PastryShopDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PastryShop.Models;
+
+public class PastryShopDbContext : DbContext
+{
+    public PastryShopDbContext(DbContextOptions<PastryShopDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Pie> Pies { get; set; }
+    public DbSet<Category> Categories { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
 using PastryShop.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
 builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+builder.Services.AddDbContext<PastryShopDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PastryShopConnectionString"))
+);
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
