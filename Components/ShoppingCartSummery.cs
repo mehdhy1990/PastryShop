a2e29cbf9ae033a6e2f9c09a984824c61633120c
Mehdi Heidari <hymehdi84@gmail.com>
2025-02-08 15:51:54 +0100

    shopping cart view component created

---
+++ Components/ShoppingCartSummery.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using PastryShop.Models;
+using PastryShop.ViewModel;
+
+namespace PastryShop.Components;
+
+public class ShoppingCartSummery : ViewComponent
+{
+    private readonly IShoppingCart _shoppingCart;
+
+    public ShoppingCartSummery(IShoppingCart shoppingCart)
+    {
+        _shoppingCart = shoppingCart;
+    }
+
+    public IViewComponentResult Invoke()
+    {
+        var items = _shoppingCart.GetShoppingCardItems();
+        _shoppingCart.ShoppingCartItems = items;
+        var shopingcartViewModel = new ShoppingCartViewModel(_shoppingCart,_shoppingCart.GetShoppingCartTotal());
+        return View(shopingcartViewModel);
+    }
+}
