478a6172ab0af5cd47697c7c6abd7b76054d4163
Mehdi Heidari <hymehdi84@gmail.com>
2025-02-08 12:19:52 +0100

    shopping card and session service injected

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IPieRepository, PieRepository>();
+
+
+builder.Services.AddScoped<IShoppingCard, ShoppingCart>(sp=>ShoppingCart.GetCart(sp));
+builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddDbContext<PastryShopDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("PastryShopConnectionString"))
 );
@@ -12,6 +17,7 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 app.UseStaticFiles();
+app.UseSession();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
