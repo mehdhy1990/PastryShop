66ca09e724eb128bc5189ccb3885a3610ff6a270
Mehdi Heidari <hymehdi84@gmail.com>
2025-02-08 12:23:39 +0100

    shopping card view model and controller is made

---
+++ Controllers/ShoppingCardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PastryShop.Models;
+
+namespace PastryShop.Controllers;
+
+public class ShoppingCardController: Controller
+{
+    private readonly IPieRepository _pieRepository;
+    private readonly IShoppingCard _shoppingCard;
+
+    public ShoppingCardController(IPieRepository pieRepository, IShoppingCard shoppingCard)
+    {
+        _pieRepository = pieRepository;
+        _shoppingCard = shoppingCard;
+    }
+}
---
+++ ViewModels/ShoppingCardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using PastryShop.Models;
+
+namespace PastryShop.ViewModel;
+
+public class ShoppingCardViewModel
+{
+    public IShoppingCard ShoppingCard { get; set; }
+    public decimal ShoppingCardTotal { get; set; }
+
+    public ShoppingCardViewModel(IShoppingCard shoppingCard, decimal shoppingCardTotal)
+    {
+        ShoppingCard = shoppingCard;
+        ShoppingCardTotal = shoppingCardTotal;
+    }
+}
