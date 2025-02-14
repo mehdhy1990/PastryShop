37daf20e5ace3e154d30b44c9cef368339149ff8
Mehdi Heidari <hymehdi84@gmail.com>
2025-02-14 14:13:11 +0100

    search controller added

---
+++ Controllers/Api/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PastryShop.Models;
+
+namespace PastryShop.Controllers.Api;
+[Route("api/[controller]")]
+[ApiController]
+public class SearchController : ControllerBase
+{
+    private readonly IPieRepository _pieRepository;
+
+    public SearchController(IPieRepository pieRepository)
+    {
+        _pieRepository = pieRepository;
+    }
+[HttpGet]
+    public IActionResult GetAll()
+    {
+        
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetPiesById(int id)
+    {
+        
+    }
+    
+}
